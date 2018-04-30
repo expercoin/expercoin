fd6d46ac75d215f7c71fe12abb02d5761c90b7fe
Almin Karic <almin_k@live.com>
2018-04-30 12:50:35 +0200

    added: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "parser": "typescript-eslint-parser",
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
