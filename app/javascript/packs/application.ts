65c6e2ff5d4741bea8c9b23f1361cc178544cd0c
SedadKosovac <sedad@miavision.net>
2018-04-30 11:27:23 +0200

    modal and select vue plugin

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -4,15 +4,33 @@ import Vuex from 'vuex'
 import App from '../app.vue'
 import Hello from '../hello.vue'
 import Split from '../split.vue'
+import VModal from 'vue-js-modal'
+import vSelect from 'vue-select'
 
+Vue.use(VModal)
 Vue.use(TurbolinksAdapter)
 Vue.use(Vuex)
+
+Vue.component('v-select', vSelect)
 
 document.addEventListener('turbolinks:load', () => {
   const app = new Vue({
     el: '#root',
     data: {
-      message: "Im Root Component I call components"
+      message: "Im Root Component I call components",
+      selected: 'value'
+    },
+    methods: {
+      show () {
+        this.$modal.show('hello-world');
+      },
+      hide () {
+        this.$modal.hide('hello-world');
+      },
+      submit () {
+        console.log('submit');
+        this.$modal.hide('hello-world');
+      }
     },
     components: { App, Hello, Split }
   })
