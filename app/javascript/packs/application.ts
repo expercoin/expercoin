89527834aba2f6b637f61a643d764d219bb9f2da
sedadk <sedad@miavision.net>
2018-05-04 14:32:09 +0200

    one vue instance per layout application include all components

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -1,3 +1,30 @@
 import './plugins_vue.ts';
-import './pages/index.ts';
-import './pages/show.ts';
+import Vue from 'vue/dist/vue.esm';
+import Hello from '../components/hello.vue';
+import Split from '../components/split.vue';
+import App from '../components/app.vue';
+import toggleInput from '../components/toggleInput.vue';
+import vSelect from 'vue-select';
+
+Vue.component('v-select', vSelect)
+Vue.component('app', App)
+Vue.component('split', Split)
+Vue.component('hello', Hello)
+Vue.component('toggle-input', toggleInput)
+
+document.addEventListener('turbolinks:load', () => {
+  const app = new Vue({
+    el: '[data-behaviour="vue"]',
+    data: {
+      selected: ''
+    },
+    methods: {
+      show() {
+      },
+      hide() {
+      },
+      submit() {
+      }
+    }
+  })
+})
