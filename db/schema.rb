1be06526e3d940d9d05a4560cdb9212aa09f927b
Almin Karic <almin_k@live.com>
2018-05-08 11:09:13 +0200

    carrierwave gem added and photo uploader

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2018_05_08_082713) do
     t.string "last_name"
     t.string "title"
     t.integer "rate"
+    t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
