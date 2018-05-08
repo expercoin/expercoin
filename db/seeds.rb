c5ad7bf917830482597c918655bf9d98918d06b6
sedadk <sedad@miavision.net>
2018-05-08 13:03:29 +0200

    devise sign up

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-User.create!(email: 'johnsnow@got.com', password: '123456', password_confirmation: '123456') if Rails.env.development?
-Profile.create!(user: User.first, first_name: 'John', last_name: 'Snow', title: 'King in the North', rate: 200) if Rails.env.development?
+User.create!(first_name: 'John', last_name: 'Snow', email: 'johnsnow@got.com', password: '123456', password_confirmation: '123456') if Rails.env.development?
+# Profile.create!(user: User.first, first_name: 'John', last_name: 'Snow', title: 'King in the North', rate: 200) if Rails.env.development?
 Category.create!(name: 'Business', description: 'Get advice on all aspects of business from international strategy to leading a team of innovators.') if Rails.env.development?
