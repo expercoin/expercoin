63d0357f6a9b13491eabbef35563d705f06d1538
sedadk <sedad@miavision.net>
2018-05-08 09:48:32 +0200

    category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_08_073629) do
+ActiveRecord::Schema.define(version: 2018_05_08_074050) do
+
+  create_table "categories", force: :cascade do |t|
+    t.integer "parent_id"
+    t.string "name"
+    t.text "banner"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
