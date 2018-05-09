64538b73ea952bb90946eee6bee6428351e327c8
Almin Karic <almin_k@live.com>
2018-05-09 11:25:14 +0200

    added review model

---
+++ db/migrate/20180509091347_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.string :title
+      t.string :message
+      t.integer :rate
+      t.integer :profile_id
+      t.integer :author_id
+
+      t.timestamps
+    end
+  end
+end
