9b5ba93fba77ff9159b701aef1aa5d8f3f624888
Enes Smajic <enes@miavision.net>
2018-05-10 16:41:25 +0200

    Redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    dashboard_index_path
+    calls_path
   end
 
   protected
