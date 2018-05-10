8f616d7dae79bee13f4cbed9fa061f5ae7952234
Almin Karic <almin_k@live.com>
2018-05-10 16:15:34 +0200

    banner uploader

---
+++ app/uploaders/banner_uploader.rb
@@ -0,0 +1,30 @@
+class BannerUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  def content_type_whitelist
+    /image\//
+  end
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  process resize_to_fit: [1500, 500]
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
+end
