74512929e8a31573b253e069479f22c2112c178e
sedadk <sedad@miavision.net>
2018-05-10 11:33:00 +0200

    calls controller change

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -22,6 +22,7 @@ class RequestsController < ApplicationController
 
   def update
     return render :edit unless @request.update(request_params)
+    @request.update_status
     render :thankyou
   end
 
@@ -34,10 +35,6 @@ class RequestsController < ApplicationController
   end
 
   private
-
-  def set_profile
-    @profile = Profile.friendly.find(params[:expert])
-  end
 
   def request_params
     params.require(:request).permit(request_attributes)
