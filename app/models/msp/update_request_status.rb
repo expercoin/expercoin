f5283f4b38e54c5993da2da507d4ccfd7ca064f8
sedadk <sedad@miavision.net>
2018-05-14 11:41:16 +0200

    request draft status

--- app/models/msp/update_request_status.rb
+++ app/models/msp/update_request_status.rb
@@ -5,15 +5,26 @@ module MSP
     end
 
     def perform
+      pending_status
       accepted_status
       completed_status
     end
 
     private
- 
+    
+    def pending_status
+      return unless valid_for_pending_status?
+      @request.update(status: 'pending')
+
+    end
+
+    def valid_for_pending_status?
+      @request.draft?
+    end
+
     def accepted_status
       return unless valid_for_accepted_status?
-      @request.update(status: 1)
+      @request.update(status: 'accepted')
     end
 
     def valid_for_accepted_status?
@@ -22,7 +33,7 @@ module MSP
 
     def completed_status
       return unless valid_for_completed_status?
-      @request.update(status: 2)
+      @request.update(status: 'completed')
     end
 
     def valid_for_completed_status?
