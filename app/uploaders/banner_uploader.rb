7aaecff205fd6b6a309c40a139d6059f73d4cb3a
Almin Karic <almin_k@live.com>
2018-05-28 16:20:43 +0200

    old images settings

--- app/uploaders/banner_uploader.rb
+++ app/uploaders/banner_uploader.rb
@@ -4,7 +4,7 @@ class BannerUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "app/assets/images/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def extension_whitelist
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,4 +1,3 @@
 CarrierWave.configure do |config|
   config.remove_previously_stored_files_after_update = false
-  config.root = Rails.root
 end
