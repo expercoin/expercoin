13d15fe1d5cac4896e86fc7d4eba4206c3ee243b
sedadk <sedad@miavision.net>
2018-06-04 14:32:54 +0200

    conditional action button on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   layout 'dashboard'
 
-  def index; 
-
+  def index
+    @calls = current_user.profile.requests.count > current_user.profile.created_requests.count
   end
 end
