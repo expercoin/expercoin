152b261b3d0843971385c401347f316ff4d88835
Almin Karic <almin_k@live.com>
2018-06-06 10:31:55 +0200

    address in verify

--- app/controllers/requests/verify_controller.rb
+++ app/controllers/requests/verify_controller.rb
@@ -2,6 +2,7 @@ module Requests
   class VerifyController < ApplicationController
     before_action :authenticate_user!
     before_action :set_request
+    before_action :set_address
 
     def index
     end
@@ -12,6 +13,11 @@ module Requests
 
     def set_request
       @request = Request.find(params[:request_id])
+    end
+
+    def set_address
+      wallet = @request.requester.wallet
+      @address = wallet.eth_addresses.find_by(default: true) || wallet.eth_addresses.last
     end
   end
 end
