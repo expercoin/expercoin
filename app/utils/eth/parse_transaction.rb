61ddcb36effde4233e32ebbae90dbb1b881e7806
sedadk <sedad@miavision.net>
2018-06-06 16:44:41 +0200

    eth foramter

--- app/utils/eth/parse_transaction.rb
+++ app/utils/eth/parse_transaction.rb
@@ -1,29 +1,34 @@
-class ParseTransaction
-  def initialize(transaction)
-    @transaction = transaction
-    @params = {}
-  end
+module Eth
+  class ParseTransaction
+    def initialize(transaction)
+      @transaction = transaction
+      @params = {}
+    end
 
-  def perform
-    @params['tx_hash'] = tx_hash
-    @params
-  end
+    def perform
+      @params['tx_hash'] = tx_hash
+      @params
+    end
 
-  private
+    def value
+      Ethereum::Formatter.new
+                        .yield_self { |it| it.output_to_int(@transaction['value']) }
+    end
 
-  def from
-    @transaction['from']
-  end
+    def from
+      @transaction['from']
+    end
 
-  def to
-    @transaction['to']
-  end
+    def to
+      @transaction['to']
+    end
 
-  def tx_hash
-    @transaction['hash']
-  end
+    def tx_hash
+      @transaction['hash']
+    end
 
-  def block_number
-    @transaction['blockNumber']
+    def block_number
+      @transaction['blockNumber']
+    end
   end
 end
