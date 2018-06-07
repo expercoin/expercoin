902df1baaf79d12277fcda23f0890d85070ba31f
Almin Karic <almin_k@live.com>
2018-06-07 12:12:03 +0200

    fix calls issue

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,5 @@ class DashboardController < ApplicationController
   layout 'dashboard'
 
   def index
-    @calls = current_user.profile.requests.count > current_user.profile.created_requests.count
   end
 end
