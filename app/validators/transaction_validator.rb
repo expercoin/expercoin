82cc8e2d91eb7dcc7a60a5edac271585c1d2f3e8
Almin Karic <almin_k@live.com>
2018-06-07 14:30:58 +0200

    validator improvement

--- app/validators/transaction_validator.rb
+++ app/validators/transaction_validator.rb
@@ -25,14 +25,6 @@ class TransactionValidator < BaseValidator
     request.expert.expercoin_rate * request.requested_length.to_i
   end
 
-  def tx_hash
-    @transaction.tx_hash
-  end
-
-  def required_amount
-    @transaction.block_number
-  end
-
   def required_amount
     return errors.add(:eth_amount, 'Must be valid') if amount_to_pay > @transaction.eth_amount
   end
