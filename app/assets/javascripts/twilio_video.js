2c2f143ed40b886688113ebb782a2d1c5478ba21
sedadk <sedad@miavision.net>
2018-06-11 14:08:22 +0200

    trigger form submit on particpaint connected

--- app/assets/javascripts/twilio_video.js
+++ app/assets/javascripts/twilio_video.js
@@ -126,7 +126,7 @@ function videoTwilioInitialize(twilo=2){
     Twilio.Video.connect(token).then(function(room) {
       window.room = room;
       room.on('participantConnected', function(participant) {
-        console.log('sssss')
+        $('#session-time-form [type="submit"]').click();
         resetTracks();
       })
 
