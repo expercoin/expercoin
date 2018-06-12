eaeb873398a1a1fd37883db95c5c76cd8a09d9fd
Almin Karic <almin_k@live.com>
2018-06-12 14:31:38 +0200

    profile spec

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,24 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:profile) { create(:profile) }
+
+  describe 'Factory' do
+    it { expect(profile).to be_valid }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+  end
+
+  describe 'Associations' do
+    it { expect(profile).to belong_to(:user) }
+    it { expect(profile).to have_many(:reviews) }
+    it { expect(profile).to have_many(:created_reviews) }
+    it { expect(profile).to have_and_belong_to_many(:categories) }
+    it { expect(profile).to have_many(:requests) }
+  end
 end
