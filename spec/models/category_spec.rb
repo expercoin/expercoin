4260a292b23b70142f0160ffb61b5337f09a419c
Almin Karic <almin_k@live.com>
2018-06-12 16:34:39 +0200

    improved category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,12 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  let(:category) { build(:category) }
-  let(:category_with_banner) { build(:category, :with_banner) }
+  let(:category) { create(:category) }
 
   describe 'Factory' do
     it { expect(category).to be_valid }
-    it { expect(category_with_banner).to be_valid }
   end
 
   describe 'Validations' do
@@ -15,5 +15,17 @@ RSpec.describe Category, type: :model do
 
   describe 'Associations' do
     it { expect(category).to have_and_belong_to_many(:profiles) }
+    it { expect(category).to have_many(:categories) }
+    it { expect(category).to belong_to(:parent) }
+  end
+
+  describe 'Columns' do
+    it { should have_db_column :slug }
+    it { should have_db_column :banner }
+  end
+
+  describe 'Friendly find' do
+    let(:find_category) { Category.friendly.find(category.id) }
+    it { expect(find_category).not_to be_nil }
   end
 end
