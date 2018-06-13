864b71cf2fbc7e153bf4449598d350841f8a3387
Almin Karic <almin_k@live.com>
2018-06-13 11:44:57 +0200

    profile form specs

---
+++ spec/forms/profile_form_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ProfileForm do
+  let(:profile) { create(:profile) }
+  let(:category_ids) do
+    4.times { create(:category) }
+    Category.all.map(&:id)
+  end
+  let(:profile_form) { ProfileForm.new(profile, category_ids: category_ids) }
+
+  describe '.initialize' do
+    it 'does rate conversion' do
+      expect(profile_form.rate).to eq Eth::ValueFormatter.new(profile.rate).from_hex
+    end
+    it 'does expercoin rate conversion' do
+      expect(profile_form.expercoin_rate).to eq Eth::ValueFormatter.new(profile.expercoin_rate).from_hex
+    end
+  end
+
+  describe '.update_categories' do
+    before { profile_form.update_categories(profile) }
+    it { expect(profile.categories.count).to eq category_ids.count }
+  end
+end
