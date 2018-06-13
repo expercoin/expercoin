d25927bbc57df0d1554742c8571489597a3e6082
Almin Karic <almin_k@live.com>
2018-06-13 10:48:25 +0200

    eth address factory and spec

--- spec/factories/eth_addresses.rb
+++ spec/factories/eth_addresses.rb
@@ -1,6 +1,13 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :eth_address do
-    public_key "MyText"
-    wallet nil
+    public_key "0x#{Faker::Crypto.sha1}"
+    default false
+    wallet
+
+    trait :default do
+      default true
+    end
   end
 end
