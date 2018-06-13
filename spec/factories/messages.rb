c2fe63e788d04dc42d61e25d535e74eb4140ff09
Almin Karic <almin_k@live.com>
2018-06-13 10:36:23 +0200

    messages factory and spec

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,9 +1,20 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :message do
-    sender_id 1
-    receiver_id 1
-    parent_id 1
-    title "MyString"
-    body "MyText"
+    title Faker::Hacker.say_something_smart
+    body Faker::Lorem.paragraph(2, true, 20)
+
+    sender do
+      User.first || create(:user, email: 'sender@message.com')
+    end
+
+    receiver do
+      User.second || create(:user, email: 'receiver@message.com')
+    end
+
+    trait :with_parent do
+      parent { create(:message) }
+    end
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:message) { build(:message) }
+  let(:message_with_parent) { build(:message, :with_parent) }
+
+  describe 'Factory' do
+    it { expect(message).to be_valid }
+    it { expect(message_with_parent).to be_valid }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:title) }
+  end
+
+  describe 'Associations' do
+    it { expect(message).to belong_to(:sender) }
+    it { expect(message).to belong_to(:receiver) }
+    it { expect(message).to belong_to(:parent) }
+  end
 end
