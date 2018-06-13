8a11587145373424baa6ffd467b76ddb06a7a190
Almin Karic <almin_k@live.com>
2018-06-13 10:21:05 +0200

    review factory and spec

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,22 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:review) { build(:review) }
+
+  describe 'Factory' do
+    it { expect(review).to be_valid }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:rate) }
+    it { should validate_presence_of(:message) }
+  end
+
+  describe 'Associations' do
+    it { expect(review).to belong_to(:profile) }
+    it { expect(review).to belong_to(:author) }
+    it { expect(review).to belong_to(:request) }
+  end
 end
