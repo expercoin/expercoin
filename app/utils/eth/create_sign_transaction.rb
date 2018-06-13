c164022e8d3330ad14501b9cc0c3961f82dffb16
sedadk <sedad@miavision.net>
2018-06-13 16:35:55 +0200

    accepte ether and hex

--- app/utils/eth/create_sign_transaction.rb
+++ app/utils/eth/create_sign_transaction.rb
@@ -18,8 +18,12 @@ module Eth
     end
 
     def wei_amount
-      # (@amount.to_f * 1_000_000_000_000_000_000.0).to_i
-      @amount.hex
+      return @amount.hex if hex_value
+      (@amount.to_f * 1_000_000_000_000_000_000.0).to_i
+    end
+
+    def hex_value
+      @amount.to_s&.downcase&.starts_with?('0x')
     end
 
     def sign_params
@@ -27,14 +31,16 @@ module Eth
         key: ENV['PRIVATE_KEY'],
         address: @address,
         amount: wei_amount,
-        from: ENV['ETH_ADDRESS']
+        from: ENV['ETH_ADDRESS'],
+        api_key: ENV['INFURA']
       }
     end
 
     def request
       require "uri"
       require "net/http"
-       Net::HTTP.post_form(
+
+      Net::HTTP.post_form(
         URI.parse(ENV['LOCAL_EXPRESS_SERVER']),
         sign_params
       ).yield_self(&:body)
