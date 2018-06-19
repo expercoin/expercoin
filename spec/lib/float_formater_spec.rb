e03b33171f50097e8d44c61690f9e74db6a2b307
sedadk <sedad@miavision.net>
2018-06-19 16:17:49 +0200

    user spec and float spec refactor

--- spec/lib/float_formater_spec.rb
+++ spec/lib/float_formater_spec.rb
@@ -3,26 +3,31 @@
 require 'rails_helper'
 
 RSpec.describe FloatFormater do
-  let(:formated_number) { 0.0001 }
-  let(:long_formated_number) { 0.0000133 }
-  let(:unformated_number) { 0.000434341 }
-  let(:formated_number_instance) { FloatFormater.new(formated_number) }
-  let(:long_formated_number_instance) { FloatFormater.new(long_formated_number) }
-  let(:unformated_number_instance) { FloatFormater.new(unformated_number) }
+  VALUES = [
+    {
+      key: :formated, val: 0.0001, result: 0.0001,
+      desc: 'returns passed value if correct format'
+    },
+    {
+      key: :long_formated, val: 0.0000133, result: 1.33e-05,
+      desc: 'returns passed Euler number formated value if correct format'
+    },
+    {
+      key: :unformated, val: 0.000434341, result: '0.00043...',
+      desc: 'returns formated value'
+    }
+  ].freeze
+
+  VALUES.each do |obj|
+    let("#{obj[:key]}_number") { obj[:val] }
+    let("#{obj[:key]}_number_instance") { FloatFormater.new(send("#{obj[:key]}_number")) }
+
+    describe obj[:desc] do
+      it { expect(send("#{obj[:key]}_number_instance").with_dots).to eq obj[:result] }
+    end
+  end
 
   describe '.initialize' do
     it { expect { formated_number_instance }.not_to raise_error }
-  end
-
-  describe 'returns passed value if correct format' do
-    it { expect(formated_number_instance.with_dots).to eq 0.0001 }
-  end
-
-  describe 'returns passed Euler number formated value if correct format' do
-    it { expect(long_formated_number_instance.with_dots).to eq 1.33e-05 }
-  end
-
-  describe 'returns formated value' do
-    it { expect(unformated_number_instance.with_dots).to eq '0.00043...' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,20 +4,30 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { build(:user) }
+  ASSOCIATIONS = [
+    { model: :profile, association: 'have_one' },
+    { model: :messages, association: 'have_many' },
+    { model: :transactions, association: 'have_many' }
+  ].freeze
+
+  VALIDATIONS = [
+    { validation: 'validate_presence_of', attr: :email },
+    { validation: 'validate_presence_of', attr: :password }
+  ].freeze
 
   describe 'Factory' do
     it { expect(user).to be_valid }
   end
 
   describe 'Validations' do
-    it { should validate_presence_of(:email) }
-    # it { should validate_uniqueness_of(:email) }
-    it { should validate_presence_of(:password) }
+    VALIDATIONS.each do |obj|
+      it { should send(obj[:validation], obj[:attr]) }
+    end
   end
 
   describe 'Associations' do
-    it { expect(user).to have_one(:profile) }
-    it { expect(user).to have_many(:messages) }
-    it { expect(user).to have_many(:transactions) }
+    ASSOCIATIONS.each do |obj|
+      it { expect(user).to send(obj[:association], obj[:model]) }
+    end
   end
 end
