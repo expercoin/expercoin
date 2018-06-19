dcad7b7c9147a7120f026facef22b923dd777675
Almin Karic <almin_k@live.com>
2018-06-19 11:50:06 +0200

    shared examples

---
+++ spec/support/shared_examples/authenticated_user.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'authenticated user' do
+  let(:current_user) { controller.current_user }
+  it 'should authenticate user' do
+    expect(controller.authenticate_user!).not_to be_nil
+  end
+  it { expect(current_user.email).to eq user.email }
+  it { expect(response).not_to redirect_to user_session_path }
+end
