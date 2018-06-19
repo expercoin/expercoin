36bc277daee85f32bdf966a45d6c5cd812e201c1
syndrom95 <ismet.n1@live.com>
2018-06-19 15:03:40 +0200

    Calls request spec

--- spec/requests/calls_controller_spec.rb
+++ spec/requests/calls_controller_spec.rb
@@ -3,4 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe CallsController, type: :request do
+  let!(:request_made) { create(:request) }
+  let!(:user) { request_made.expert.user }
+  let!(:logged_user) { sign_in(user) }
+  let(:request_params) { build(:request) }
+
+  describe 'GET index' do
+    before { get calls_path }
+    it_behaves_like 'authenticated user'
+    it { expect(response).to have_http_status(:ok) }
+  end
+
+  describe 'GET show' do
+    before { get call_path(request_made) }
+    it_behaves_like 'authenticated user'
+    it { expect(response).to have_http_status(:ok) }
+  end
+
+  describe 'POST update' do
+    before { patch call_path(id: request_made.id, request: attributes_for(:request)) }
+    it_behaves_like 'authenticated user'
+    it { expect(response).to have_http_status(:redirect) }
+    it { expect(response).to redirect_to calls_path }
+  end
 end
