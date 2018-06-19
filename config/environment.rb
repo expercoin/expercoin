66debda5c0813f5e89bfdf54daf148f691c66255
sedadk <sedad@miavision.net>
2018-06-19 11:48:06 +0200

    dotenv gem, config

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-app_environment_variables = File.join(Rails.root, 'config', 'app_environment_variables.rb')
-load(app_environment_variables) if File.exist?(app_environment_variables)
