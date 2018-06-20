d4ae443b449b2b8e0f4d016855323d84e97204c5
sedadk <sedad@miavision.net>
2018-06-20 12:54:19 +0200

    categroy subcatergory rubycritic

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,9 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show]
+  before_action :set_category, :set_profiles, only: [:show]
   layout 'dashboard'
 
   def show
-    @profiles = @category.children_profiles
-      .except(current_user&.profile).page(params[:page]).per(9)
-    @subcategories = @category.categories
+    @subcategories = category.categories
   end
 
   def index
@@ -13,6 +11,14 @@ class CategoriesController < ApplicationController
   end
 
   private
+
+  attr_reader :category, :subcategories, :profiles
+
+  def set_profiles
+    @profiles = category.children_profiles
+                        .except(current_user.try(:profile))
+                        .page(params[:page]).per(9)
+  end
 
   def set_category
     @category = Category.main.friendly.find(params[:id])
