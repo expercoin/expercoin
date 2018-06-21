f60733b0c4707e3cc30aed15f0dcfaf89852b00c
sedadk <sedad@miavision.net>
2018-06-21 14:21:04 +0200

    job tests for on queue

--- spec/jobs/release_funds_job_spec.rb
+++ spec/jobs/release_funds_job_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ReleaseFundsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  subject(:job) { described_class.perform_later(key) }
+
+  let(:key) { 123 }
+
+  it 'queues the job' do
+    expect { job }.to have_enqueued_job(described_class)
+      .with(key)
+      .on_queue('default')
+  end
 end
