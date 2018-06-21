a8b8ee9283479fdc75af790bd91ebcd2330b5411
Almin Karic <almin_k@live.com>
2018-06-21 12:07:29 +0200

    jobs spec

--- spec/jobs/release_funds_job_spec.rb
+++ spec/jobs/release_funds_job_spec.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe ReleaseFundsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:transaction) { create(:transaction, tx_hash: nil) }
+  let!(:test_env) { ActiveJob::Base.queue_adapter = :test }
+  describe 'enque job' do
+    it 'releases funds' do
+      expect do
+        ReleaseFundsJob.perform_later(transaction)
+      end.to have_enqueued_job.with(transaction)
+    end
+  end
 end
--- spec/jobs/update_transaction_status_job_spec.rb
+++ spec/jobs/update_transaction_status_job_spec.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe UpdateTransactionStatusJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:transaction) { create(:transaction) }
+  let!(:test_env) { ActiveJob::Base.queue_adapter = :test }
+  describe 'enque job' do
+    it 'updates transactions' do
+      expect do
+        UpdateTransactionStatusJob.perform_later
+      end.to have_enqueued_job
+    end
+  end
 end
