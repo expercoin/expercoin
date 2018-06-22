feaa12cb4f9c2add1be34b108c1741f60f1151a9
Almin Karic <almin_k@live.com>
2018-06-22 10:28:13 +0200

    update experts transaction job spec

---
+++ spec/jobs/update_experts_transactions_job_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UpdateExpertsTransactionsJob, type: :job do
+  let(:tx_hash) { '0xcce351e43a4c3ed8b9e4e96652992d1a9c1f928497eb0ba470997ccc6a56f917' }
+  let(:request) { create(:request, :verifying, tx_hash: tx_hash) }
+  let(:transaction) { create(:transaction) }
+  let!(:expert_transaction) do
+    create(:transaction,
+           tx_hash: tx_hash,
+           parent: transaction,
+           receiver: transaction.request.expert.user)
+  end
+  include ActiveJob::TestHelper
+
+  subject(:job) { described_class.perform_later }
+
+  it 'queues the job' do
+    expect { job }.to have_enqueued_job(described_class).on_queue('default')
+  end
+
+  describe '.perform' do
+    before do
+      described_class.perform_now
+      perform_enqueued_jobs { job }
+      expert_transaction.reload
+    end
+    it { expect(expert_transaction.status).to eq 'completed' }
+  end
+end
