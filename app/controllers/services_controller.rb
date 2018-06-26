b13a9902e64f8fcfc0d971c879f4ea8b0ff7c5a2
sedadk <sedad@miavision.net>
2018-06-26 12:00:59 +0200

    service featured profile

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -6,5 +6,6 @@ class ServicesController < ApplicationController
   def show
     @service = Service.friendly.find(params[:id])
     @providers = @service.profiles
+    @featured_provider = @service.featured_profile
   end
 end
