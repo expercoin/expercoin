d343c4015e83d56cd5a3c58bb4ba8dc863e81f74
Almin Karic <almin_k@live.com>
2018-07-03 14:39:20 +0200

    service reviews spec

--- spec/models/service_provider_spec.rb
+++ spec/models/service_provider_spec.rb
@@ -1,5 +1,21 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe ServiceProvider, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:service_provider) { build(:service_provider) }
+  let(:featured_count) { ServiceProvider.where(featured: true).count }
+
+  describe 'Factory' do
+    it { expect(service_provider).to be_valid }
+  end
+
+  describe 'Associations' do
+    it { expect(service_provider).to belong_to :service }
+    it { expect(service_provider).to belong_to :profile }
+  end
+
+  describe 'Scopes' do
+    it { expect(described_class.featured.count).to eq featured_count }
+  end
 end
--- spec/utils/service_rating_spec.rb
+++ spec/utils/service_rating_spec.rb
@@ -3,5 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe ServiceRating do
-  pending 'service rating'
+  let(:review) { create(:review, rate: 5) }
+  let(:profile) { review.profile }
+  let!(:review2) { create(:review, rate: 2, profile: profile) }
+  let(:service) { create(:service, profiles: [profile]) }
+  let(:service_rating) { described_class.new(service) }
+
+  describe '.initialize' do
+    it { expect { service_rating }.not_to raise_exception }
+  end
+
+  describe '.average_rating' do
+    it { expect(service_rating.average_rating).to eq 3.5 }
+  end
 end
