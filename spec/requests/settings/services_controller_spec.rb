9c7ba0bd933b4605d4b2bc5fe94a66c69754576e
Almin Karic <almin_k@live.com>
2018-07-03 15:23:02 +0200

    settings services spec

---
+++ spec/requests/settings/services_controller_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Settings::ServicesController, type: :request do
+  let(:profile) { create(:profile) }
+  let(:user) { profile.user }
+  let!(:logged_user) { sign_in(user) }
+  let!(:service) { create(:service, :with_owner, profiles: [profile]) }
+
+  describe '.index' do
+    before { get settings_services_path }
+    it_behaves_like 'authenticated user'
+    it { expect(response).to have_http_status(:ok) }
+  end
+end
