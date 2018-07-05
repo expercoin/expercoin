db0c75498a701e637bc6e0615bfd0c5225f3f1cf
Almin Karic <almin_k@live.com>
2018-07-05 11:03:08 +0200

    oauth implemented

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -3,16 +3,19 @@ class OauthController < ApplicationController
     begin
       oauth = OauthService.new(request.env['omniauth.auth'])
       if oauth_account = oauth.create_oauth_account!
-        redirect_to Config.provider_login_path
+        sign_in(oauth_account.user)
+        redirect_to root_path
       end
     rescue => e
+      binding.pry
       flash[:alert] = "There was an error while trying to authenticate your account."
-      redirect_to register_path
+      redirect_to root_path
     end
   end
 
   def failure
+    binding.pry
     flash[:alert] = "There was an error while trying to authenticate your account."
-    redirect_to register_path
+    redirect_to root_path
   end
 end
