28d02aaeab5ba2c2846aea509833175da662641c
Almin Karic <almin_k@live.com>
2018-07-09 10:02:24 +0200

    wishlist specs

--- app/controllers/wishlist_controller.rb
+++ app/controllers/wishlist_controller.rb
@@ -25,20 +25,3 @@ class WishlistController < ApplicationController
     end  
   end  
 end 
-
-
-
-
-
-# @user = User.new(params[:user])
- 
-#   respond_to do |format|
-#     if @user.save
-#       format.html { redirect_to @user, notice: 'User was successfully created.' }
-#       format.js
-#       format.json { render json: @user, status: :created, location: @user }
-#     else
-#       format.html { render action: "new" }
-#       format.json { render json: @user.errors, status: :unprocessable_entity }
-#     end
-#   end
