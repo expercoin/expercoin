008d9c30cc2a168d392fc6d9885f05bdbf8fc8c8
Almin Karic <almin_k@live.com>
2018-07-10 14:32:45 +0200

    create notification class

---
+++ app/utils/notifications/create_notification.rb
@@ -0,0 +1,40 @@
+module Notifications
+  class CreateNotification
+    def initialize(params)
+      @params = params
+      @user = User.find_by(id: params[:user_id])
+    end
+
+    def perform
+      Notification.create!(
+        title: title,
+        content: content,
+        user: @user,
+        resource_type: resource_type,
+        resource_id: resource_id
+      )
+    end
+
+    private
+
+    def resource_type
+      @params[:resource_type]
+    end
+
+    def resource_id
+      @params[:resource_id]
+    end
+
+    def type
+      @params[:type]
+    end
+
+    def title
+      I18n.t("notification.#{resource_type.downcase}.#{type}.title")
+    end
+
+    def content
+      I18n.t("notification.#{resource_type.downcase}.#{type}.content")
+    end
+  end
+end
