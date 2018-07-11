e04bc96c3a05eb8171a344d682d70499ce34d781
sedadk <sedad@miavision.net>
2018-07-11 10:15:03 +0200

    message after create based on type, remove photo

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,11 +8,17 @@ class MessagesController < ApplicationController
     create_message_notification('new')
     @message.assets = current_user.assets
     current_user.assets.clear
+    send_email
   end
 
   private
 
+  def send_email
+    MessageMailer.new_message(@message).deliver_later unless @message.parent_id
+    MessageMailer.reply_message(@message).deliver_later if @message.parent_id
+  end
+
   def messages_params
-    params.require(:message).permit(:title, :body, :receiver_id, :asset).merge(sender: current_user)
+    params.require(:message).permit(:parent_id, :title, :body, :receiver_id, :asset).merge(sender: current_user)
   end
 end
--- spec/mailers/previews/message_mailer_preview.rb
+++ spec/mailers/previews/message_mailer_preview.rb
@@ -1,12 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/message_mailer
 class MessageMailerPreview < ActionMailer::Preview
   def new_message
-    message = Message.where(parent: nil).sample
+    message = Message.where(parent: nil).last
     MessageMailer.new_message(message)
   end
 
   def reply_message
-    message = Message.where.not(parent: nil).sample
+    message = Message.where.not(parent: nil).last
     MessageMailer.reply_message(message)
   end
 end
