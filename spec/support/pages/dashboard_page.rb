fc9e504ea5d7429f86143899bd8939a937ec0269
Almin Karic <almin_k@live.com>
2018-07-12 10:16:32 +0200

    dashboard spec

---
+++ spec/support/pages/dashboard_page.rb
@@ -0,0 +1,21 @@
+class DashboardPage
+  include Capybara::DSL
+
+  def initialize(url, user)
+    @url = url
+    @user = user
+    LoginModalPage.new('/', user).fill_and_submit_form
+  end
+
+  def click_my_request
+    click_on 'My Requests'
+  end
+
+  def click_my_calls
+    click_on 'My Calls'
+  end
+
+  def click_inbox
+    click_on 'Inbox'
+  end
+end
