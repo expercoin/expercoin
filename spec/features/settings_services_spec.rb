789e0da982d374ce124c34f775a18b812b4e47a2
Almin Karic <almin_k@live.com>
2018-07-16 11:32:58 +0200

    update service settings spec

--- spec/features/settings_services_spec.rb
+++ spec/features/settings_services_spec.rb
@@ -5,15 +5,14 @@ require 'rails_helper'
 RSpec.feature 'Settings Services', type: :system do
   let(:profile) { create(:profile) }
   let(:user) { profile.user }
-  let(:service) { create(:service, owner: profile) }
+  let!(:service) { create(:service, owner: profile) }
+  let(:service_eth_price) { ServiceDecorator.new(service).display_rate }
+  let(:service_usd_price) { ServiceDecorator.new(service).display_rate_in_usd }
 
   subject(:settings_service_page) { Settings::ServicePage.new(settings_services_path, user) }
 
-  feature 'Services' do
-    before do
-      service
-      settings_service_page.open
-    end
+  feature 'Services Actions' do
+    before { settings_service_page.open }
 
     scenario 'Opens new service page' do
       settings_service_page.open_new_service_page
@@ -29,5 +28,12 @@ RSpec.feature 'Settings Services', type: :system do
       settings_service_page.delete_service
       expect(page.body).not_to include service.title
     end
+  end
+
+  feature 'Service Values' do
+    before { settings_service_page.open }
+    it { expect(page.body).to include service_eth_price.to_s }
+    it { expect(page.body).to include "$#{service_usd_price}" }
+    it { expect(page.body).to include service.status.titleize }
   end
 end
