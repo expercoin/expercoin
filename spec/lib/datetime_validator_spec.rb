22ca4054054f2a1edd79a211b68e11b4d3d40a57
sedadk <sedad@miavision.net>
2018-07-18 11:01:12 +0200

    validate range class

--- spec/lib/datetime_validator_spec.rb
+++ spec/lib/datetime_validator_spec.rb
@@ -3,9 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe DatetimeValidator do
-  let(:correct) { described_class.new(Time.now, {start_date: Time.now, end_date: Time.now + 10.minutes }) }
+  let(:time) { Time.now }
+  let(:range) { {start_date: time, end_date: time + 10.minutes } }
+  let(:correct) { described_class.new(time, range) }
+  let(:false) { described_class.new(time - 10.minutes, range) }
 
   describe '.initialize' do
     it { expect { correct }.not_to raise_error }
+  end
+
+  describe '.in_range' do
+    it { expect(correct.in_range).to eq true }
   end
 end
