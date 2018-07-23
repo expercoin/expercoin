d233859a5369585710c52fb8af4b8fe75522efb9
Almin Karic <almin_k@live.com>
2018-07-23 14:56:32 +0200

    redirect back linkedin

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -4,7 +4,7 @@ class OauthController < ApplicationController
       oauth = OauthService.new(request.env['omniauth.auth'], current_user)
       if oauth_account = oauth.create_oauth_account!
         sign_in(oauth_account.user)
-        redirect_to root_path
+        redirect_to request.env['omniauth.origin'] || root_path
       end
     rescue => e
       flash[:alert] = "There was an error while trying to authenticate your account."
