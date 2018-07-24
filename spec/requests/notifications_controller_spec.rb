18daae48190de07d7aee66e8e0eb3836a3dbc238
Almin Karic <almin_k@live.com>
2018-07-24 16:39:05 +0200

    notification controller spec

--- spec/requests/notifications_controller_spec.rb
+++ spec/requests/notifications_controller_spec.rb
@@ -3,5 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe NotificationsController, type: :request do
-  pending 'notificiations spec'
+  let(:notification) { create(:notification, :request) }
+  let(:user) { notification.user }
+  let!(:logged_user) { sign_in(user) }
+
+  describe 'GET show' do
+    before do
+      get notification_path(notification)
+      notification.reload
+    end
+    it_behaves_like 'authenticated user'
+    it { expect(response).to redirect_to call_path(notification.resource) }
+    it { expect(notification.status).to eq 'read' }
+  end
 end
